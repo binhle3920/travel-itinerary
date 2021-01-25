1e39d7283e104fbdf252134a29ee21a5705eae48
Nguyeen Trusc <18127055@student.hcmus.edu.vn>
2021-01-25 21:21:19 +0700

    Update db.js
    confirm_user

--- utils/db.js
+++ utils/db.js
@@ -6,8 +6,29 @@ const db_config = require('../config/config.json').pg;
 const pool = new Pool(db_config);
 
 module.exports = {
-    load(sql) {
-        pool.query('sql', (error, results) => {
+    select_user(username) {
+        const q_user = `SELECT * FROM USER WHERE username = '${username}'`
+        pool.query(q_user, (error, results) => {
+            if (error) {
+                throw error
+            }
+            return results.rows;
+        })
+    }, // return null -> user not exist
+
+    select_email(email){
+        const q_email = `SELECT * FROM USER WHERE email = '${email}'`
+        pool.query(q_email, (error, results) => {
+            if (error) {
+                throw error
+            }
+            return results.rows;
+        })
+    }, // return null -> user not exist
+
+    select_pass(username){
+        const q_pass = `SELECT password FROM USER WHERE username = '${username}'`
+        pool.query(q_pass, (error, results) => {
             if (error) {
                 throw error
             }
