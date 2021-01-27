72ad47405526c19bd24a53ad676989e232dcb0bd
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2021-01-27 09:49:14 +0700

    Upgrade pg version

--- middlewares/session.mdw.js
+++ middlewares/session.mdw.js
@@ -4,7 +4,7 @@ const { pg } = require('../config/config.json');
 
 module.exports = function (app) {
   const sessionStore = new MyPgStore({
-    conString: "postgres://shjxenspysdldz:a681d453611cc91a7274db3ea7b356e156b28da1af6cc140081668d5bbafd807@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d2cetqcshjn6gn?sslmode=require",
+    conString: "postgres://shjxenspysdldz:a681d453611cc91a7274db3ea7b356e156b28da1af6cc140081668d5bbafd807@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d2cetqcshjn6gn?ssl=true&sslmode=require",
     ssl: {
       rejectUnauthorized: false
     },
