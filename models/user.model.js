1fb684ebd619ee13c55f2a29485216f2370c1dbb
nt2505 <nth250520@gmail.com>
2021-01-28 22:01:47 +0700

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const db = require("../utils/db");
 
 module.exports = {
+    // Check username
     async select_user(username) {
         const sql = `SELECT * FROM public."USER" WHERE username = '${username}'`;
         try {
@@ -12,6 +13,7 @@ module.exports = {
         }
     },
 
+    // Check email
     async select_user_by_email(email) {
         const sql = `SELECT * FROM public."USER" WHERE email = '${email}'`;
         try {
@@ -23,11 +25,48 @@ module.exports = {
         }
     },
     
+    // Thêm 1 user
     async insert_user(arr_value) {
-        const arr_querry = `('${arr_value.username}', '${arr_value.email}', '${arr_value.password}', '${arr_value.fullname}', '${arr_value.dob}', '${arr_value.type}')`;
+        const arr_querry = `('${arr_value.username}', '${arr_value.email}', '${arr_value.password}', '${arr_value.fullname}', '${arr_value.dob}', '${arr_value.type}', '${arr_value.google}')`;
         try {
             var result = await db.add("USER", arr_querry);
             return true;
+        } catch(e) {
+            console.log(e);
+            return false;
+        }
+    },
+
+    // Update new password
+    async update_password(username, newpass) {
+        const sql = `UPDATE public."USER" SET "password"= '${newpass}' WHERE "username" = '${username}'`
+        try {
+            var result =  await db.update(sql);
+            return result.rows[0];
+        } catch(e) {
+            console.log(e);
+            return false;
+        }
+    },
+
+    // Update new fullname
+    async update_fullname(username, newfullname) {
+        const sql = `UPDATE public."USER" SET "fullname"= '${newfullname}' WHERE "username" = '${username}'`
+        try {
+            var result =  await db.update(sql);
+            return result.rows[0];
+        } catch(e) {
+            console.log(e);
+            return false;
+        }
+    },
+
+    // Update new dob
+    async update_dob(username, newdob) {
+        const sql = `UPDATE public."USER" SET "dob"= '${newdob}' WHERE "username" = '${username}'`
+        try {
+            var result =  await db.update(sql);
+            return result.rows[0];
         } catch(e) {
             console.log(e);
             return false;
