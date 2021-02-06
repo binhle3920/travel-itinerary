d464b8cbf25876d57eb3f58b756b779a33917a27
nt2505 <nth250520@gmail.com>
2021-02-06 11:00:55 +0700

    update destinations view

--- routes/destinations.route.js
+++ routes/destinations.route.js
@@ -4,14 +4,14 @@ var router = express.Router();
 var desDb = require('../models/khuvuc.model');
 
 router.get('/', async function(req, res) {
-    var des = desDb.select_allkv()
+    var des = await desDb.select_allkv()
 
     var auth = req.session.auth;
     var user = null;
 
     if (auth == true) 
         user = req.session.authUser;
-  
+
     res.render('destinations/destinations', { 
         auth: auth,
         user: user,
