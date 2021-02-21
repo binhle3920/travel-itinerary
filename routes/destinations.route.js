4a5c8c804582e189590511b6202d994dc7384915
nt2505 <nth250520@gmail.com>
2021-02-21 17:25:49 +0700

    add diadiems of a destination

--- routes/destinations.route.js
+++ routes/destinations.route.js
@@ -2,13 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 var desDb = require('../models/khuvuc.model');
+var ddDb = require('../models/diadiem.model');
 
 router.get('/:CODE', async function(req, res) {
     var detail = await desDb.select_alldiadiem(req.params.CODE)
     var detail_name = await desDb.select_kvname(req.params.CODE)
     var img_link = await desDb.select_khuvucimgs(req.params.CODE)
     var kv_descrip_rate = await desDb.select_khuvuc(req.params.CODE)
-        
+    var arr_dd_img = []
+    var temp = ''
+    
+    for (var d = 0; d < detail.length; d++)
+    {
+        temp = await ddDb.select_img(detail[d].TENDD)
+        arr_dd_img.push(temp.IMGLINK)
+    }
+
     if (detail_name != null)
     {
         detail_name = detail_name.NAME
@@ -26,7 +35,8 @@ router.get('/:CODE', async function(req, res) {
         detail: detail,
         name: detail_name,
         img_link: img_link,
-        kv_descrip_rate: kv_descrip_rate
+        kv_descrip_rate: kv_descrip_rate,
+        arr_dd_img: arr_dd_img
     });
 })
 
